7e6ad03152d2cfd4156e8d8759044456efe26cf5
Priklen <igorsamodumskij@gmail.com>
2026-08-14 21:53:34 +0300

    complete

--- lua/gruber-darker/highlights/compile-mode.lua
+++ lua/gruber-darker/highlights/compile-mode.lua
@@ -1,6 +1,5 @@
 local Highlight = require("gruber-darker.highlight")
 local gruber_hl = require("gruber-darker.highlights.colorscheme").highlights
-local lsp_hl = require("gruber-darker.highlights.lsp").highlights
 
 ---@type HighlightsProvider
 local M = {
@@ -13,11 +12,18 @@ function M.setup()
 	end
 end
 
-M.highlights.compile_mode_command_output = Highlight.new("CompileModeCommandOutput", { link = lsp_hl.diagnostic_info })
-M.highlights.compile_mode_warning = Highlight.new("CompileModeWarning", { link = lsp_hl.diagnostic_warn })
-M.highlights.compile_mode_error = Highlight.new("CompileModeError", { link = lsp_hl.diagnostic_error })
-
+M.highlights.compile_mode_message = Highlight.new("CompileModeMessage", { link = gruber_hl.fg0 , underline=true})
 M.highlights.compile_mode_message_row = Highlight.new("CompileModeMessageRow", { link = gruber_hl.yellow })
 M.highlights.compile_mode_message_col = Highlight.new("CompileModeMessageCol", { link = gruber_hl.green })
+
+M.highlights.compile_mode_error = Highlight.new("CompileModeError", { link = gruber_hl.red })
+M.highlights.compile_mode_warning = Highlight.new("CompileModeWarning", { link = gruber_hl.brown })
+M.highlights.compile_mode_info = Highlight.new("CompileModeInfo", { link = gruber_hl.green })
+
+M.highlights.compile_mode_command_output = Highlight.new("CompileModeCommandOutput", { link = gruber_hl.niagara })
+M.highlights.compile_mode_directory_message = Highlight.new("CompileModeDirectoryMessage", { link = gruber_hl.niagara })
+M.highlights.compile_mode_directory_message = Highlight.new("CompileModeOutputFile", { link = gruber_hl.yellow_bold })
+M.highlights.compile_mode_directory_message = Highlight.new("CompileModeCheckResult", { link = gruber_hl.wisteria_bold })
+M.highlights.compile_mode_directory_message = Highlight.new("CompileModeCheckTarget", { link = gruber_hl.wisteria_bold })
 
 return M
